d45ab91b63063efdf9f35899c7997727a2fffbc2
Nektro <nektromc@gmail.com>
2015-12-20 15:57:52 -0500

    Update Two.js
    add function inline docs

--- js/Two.js
+++ js/Two.js
@@ -5,6 +5,7 @@ class TWO {
     this.m = mode; // switch between `2d` and `webgl`
     this.c = canvas.getContext(mode);
   }
+  // reset the canvas to be transparent and blank
   clear() {
     if (this.m === '2d')
       this.c.clearRect(0, 0, this.c.canvas.width, this.c.canvas.height);
@@ -16,6 +17,7 @@ class TWO {
       this.c.clear(this.g.COLOR_BUFFER_BIT | this.g.DEPTH_BUFFER_BIT);
     }
   }
+  // shape, mode, color, x, y, extra params...
   draw(s, m, c, x, y, p1, p2) {
     if (this.m === '2d') {
       this.c.beginPath();
@@ -34,9 +36,6 @@ class TWO {
     if (this.m === 'webgl') // TODO
       console.log(arguments);
   }
-  
-  // // // //
-  
   // draw text on the screen
   drawText(color, x, y, text) {
     if (this.m === '2d') {
@@ -49,26 +48,26 @@ class TWO {
   // draw an image to the screen
   drawImage(img, a, b, c, d, e, f, g, h) {
     if (this.m === '2d') {
-    // error prevention
-    if (img !== null) {
-      // make it one big function by scanning for parameters
-      switch (arguments.length) {
-        case 3:
-          // image, x, y
-          this.c.drawImage(img, a, b);
-          break;
-        case 5:
-          // image, x, y, w, h
-          this.c.drawImage(img, a, b, c, d);
-          break;
-        case 9:
-          // image, sx, sy, sw, sh, dx, dy, dw, dh
-          this.c.drawImage(img, a,b,c,d,e,f,g,h);
-          break;
-        default:
-          console.error("Invalid amount of parameters: " + arguments.length);
+      // error prevention
+      if (img !== null) {
+        // make it one big function by scanning for parameters
+        switch (arguments.length) {
+          case 3:
+            // image, x, y
+            this.c.drawImage(img, a, b);
+            break;
+          case 5:
+            // image, x, y, w, h
+            this.c.drawImage(img, a, b, c, d);
+            break;
+          case 9:
+            // image, sx, sy, sw, sh, dx, dy, dw, dh
+            this.c.drawImage(img, a,b,c,d,e,f,g,h);
+            break;
+          default:
+            console.error("Invalid amount of parameters: " + arguments.length);
+        }
       }
-    }
     }
     if (this.m === 'webgl') // TODO
       console.log(arguments);
