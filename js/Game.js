1d9d1b0d289792cf497c82934f1c148b5b293d85
Nektro <nektromc@gmail.com>
2015-12-20 15:28:10 -0500

    Update Game.js
    add render loop

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,9 @@
 "use strict";
 class Game {
+  // (int fps, function every, GameState... states)
   constructor() {
     this.fps = arguments[0];
-    this.everyE = arguments[1];
+    this.everyE = arguments[1]; // called during every state
     this.states = Array.prototype.slice.call(arguments).splice(2);
     this.activeState = Math.min(0, this.states.length);
     this.isRunning = false;
@@ -12,17 +13,19 @@ class Game {
     this.isRunning = true;
     if (this.isRunning) {
       var t = new Date().getTime();
+      this.everyE();
       if (t - this.lastTime > 1000 / this.fps) {
         this.lastTime = t;
-        this.everyE();
         this.states[this.activeState].update();
       }
+      this.states[this.activeState].render();
       var that = this;
       requestAnimationFrame(that.run.bind(this));
     }
   }
 }
 class GameState {
+  // (function update, function render)
   constructor(u,r) {
     this.update = u | function(){};
     this.render = r | function(){};
