d14dc3573dda300a66750f604e10c2525222a178
Nektro <nektromc@gmail.com>
2015-12-20 15:55:59 -0500

    Update Two.js
    add the separation of 2d and webgl drawing

--- js/Two.js
+++ js/Two.js
@@ -9,41 +9,46 @@ class TWO {
     if (this.m === '2d')
       this.c.clearRect(0, 0, this.c.canvas.width, this.c.canvas.height);
     if (this.m === 'webgl') {
-      this.g.clearColor(0.0, 0.0, 0.0, 0.0);
-      this.g.clearDepth(1.0);
-      this.g.enable(this.g.DEPTH_TEST);
+      this.c.clearColor(0.0, 0.0, 0.0, 0.0);
+      this.c.clearDepth(1.0);
+      this.c.enable(this.g.DEPTH_TEST);
+      this.c.depthFunc(this.g.LEQUAL);
+      this.c.clear(this.g.COLOR_BUFFER_BIT | this.g.DEPTH_BUFFER_BIT);
     }
   }
   draw(s, m, c, x, y, p1, p2) {
-    this.c.beginPath();
-    switch (s) {
-      case 'rect': this.c.rect(x, y, p1, p2); break;
-      default:
-        console.error('Invalid shape to draw: ' + s);
+    if (this.m === '2d') {
+      this.c.beginPath();
+      switch (s) {
+        case 'rect': this.c.rect(x, y, p1, p2); break;
+        default:
+          console.error('Invalid shape to draw: ' + s);
+      }
+      switch (m) {
+        case 'fill':   this.c.fillStyle = c;   this.c.fill();   break;
+        case 'stroke': this.c.strokeStyle = c; this.c.stroke(); break;
+        // no default for non-fill/stroke shapes
+      }
+      this.c.closePath();
     }
-    switch (m) {
-      case 'fill':   this.c.fillStyle = c;   this.c.fill();   break;
-      case 'stroke': this.c.strokeStyle = c; this.c.stroke(); break;
-      // no default for non-fill/stroke shapes
-    }
-    this.c.closePath();
+    if (this.m === 'webgl') // TODO
+      console.log(arguments);
   }
-
+  
   // // // //
-
-  // draws a rectangle
-  // color, x, y, width, height
-  fillRect(c, x, y, w, h) {
-    this.c.fillStyle = c;
-    this.c.fillRect(x,y,w,h);
-  };
+  
   // draw text on the screen
   drawText(color, x, y, text) {
-    this.c.fillStyle = color;
-    this.c.fillText(text, x, y);
+    if (this.m === '2d') {
+      this.c.fillStyle = color;
+      this.c.fillText(text, x, y);
+    }
+    if (this.m === 'webgl') // TODO
+      console.log(arguments);
   };
   // draw an image to the screen
   drawImage(img, a, b, c, d, e, f, g, h) {
+    if (this.m === '2d') {
     // error prevention
     if (img !== null) {
       // make it one big function by scanning for parameters
@@ -64,5 +69,8 @@ class TWO {
           console.error("Invalid amount of parameters: " + arguments.length);
       }
     }
+    }
+    if (this.m === 'webgl') // TODO
+      console.log(arguments);
   }
 }
